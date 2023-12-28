84517ef249f1e1b20cd8b1feec0288b9b939a348
munindra116 <yedoti.12008052@lpu.in>
2023-12-28 19:54:38 +0530

    Initial commit

---
+++ src/main/java/com/munindra/rest/webservices/restfulwebservices/jwt/JwtTokenResponse.java
@@ -0,0 +1,5 @@
+package com.munindra.rest.webservices.restfulwebservices.jwt;
+
+public record JwtTokenResponse(String token) {}
+
+
---
+++ target/classes/data.sql
@@ -0,0 +1,8 @@
+insert into todo (id, description, done, target_date,username)
+values (10001, 'Learn java', false, CURRENT_DATE(), 'munindra');
+
+insert into todo (id, description, done, target_date,username)
+values (10002, 'Learn python', false, CURRENT_DATE(), 'munindra');
+
+insert into todo (id, description, done, target_date,username)
+values (10003, 'Learn DevOps', false, CURRENT_DATE(), 'munindra');
